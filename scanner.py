bf44c80c1c3e06c00a58de07b2a0ce97ef38c12a
Simply-Cubing <113437319+Simply-Cubing@users.noreply.github.com>
2022-10-09 08:45:32 +0800

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -9,9 +9,13 @@ dictionary = PyDictionary()
 number_of_palin = 0
 
 for word in text.split():
-    if dictionary.meaning(word,True) and word == word[::-1]:
+    if dictionary.meaning(word,True) and word == word[::-1] and len(word) > 1:
         number_of_palin += 1
         empty.append(word)
 
-print(f"There are {number_of_palin} palindromes in this text, they are {empty}")
-     
+if len(empty) > 1:
+    print(f"There are {number_of_palin} palindromes in this text, they are {empty}")
+elif len(empty) == 1:
+    print(f"There is one palindrome in this text, it is '{empty[0]}'")    
+elif len(empty) == 0:
+    print("There are no palindromes in this text")      
